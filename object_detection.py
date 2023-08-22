ad9eafedd7b38ec6a3b049539d5056ca3a125511
anishganti <61173969+anishganti@users.noreply.github.com>
2023-08-22 10:25:32 -0400

    pre-trained yolo5 runs correctly

--- object_detection.py
+++ object_detection.py
@@ -1,11 +1,18 @@
 import torch
 import ssl
+import pandas as pd
 
 # create ssl certificate
 ssl._create_default_https_context = ssl._create_unverified_context
 
 # load the model
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+# prep the data
+
+# tweak the model
+
+# vsisualize the results
 
 # sample image
 imgs = ['https://ultralytics.com/images/zidane.jpg']  # batch of images
@@ -18,4 +25,3 @@ results.print()
 results.save()  # or .show()
 
 results.xyxy[0]  # img1 predictions (tensor)
-results.pandas().xyxy[0]  # img1 predictions (pandas)
